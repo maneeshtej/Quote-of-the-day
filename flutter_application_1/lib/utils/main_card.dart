dd1096bc2ae5b5b715539bb5de595e7b59e2ee50
maneeshtej <tejsaimaneeesh@gmail.com>
2023-11-19 17:48:15 +0530

    added comments removed unnecesary variabled and methods cleaned up

--- flutter_application_1/lib/utils/main_card.dart
+++ flutter_application_1/lib/utils/main_card.dart
@@ -12,12 +12,14 @@ class MainCard extends StatefulWidget {
   State<MainCard> createState() => _MainCardState();
 }
 
-String quotes = "";
+String quotes = "Press button to start";
 var i = 0;
 var quote = GetQuotes();
 getQuotesFunc() {
   quote.getQuotes();
 }
+
+var buttonText = "Press to start";
 
 bool buttonStat = false;
 
@@ -28,7 +30,12 @@ class _MainCardState extends State<MainCard> {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text(quotes),
+        Text(
+          quotes,
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
         TextButton(
           style: ButtonStyle(),
           onPressed: () {
@@ -36,6 +43,7 @@ class _MainCardState extends State<MainCard> {
               if (!buttonStat) {
                 quote.getQuotes();
                 buttonStat = !buttonStat;
+                quotes = "Refresh your life";
               } else {
                 if (i == quote.actualNotes.length - 1) {
                   quote.getQuotes();
@@ -46,19 +54,26 @@ class _MainCardState extends State<MainCard> {
               }
             });
           },
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Refresh",
-                style: TextStyle(color: Colors.black),
-              ),
-              Icon(
-                Icons.refresh,
-                color: Colors.black,
-              ),
-            ],
-          ),
+          child: (buttonStat)
+              ? const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Refresh",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(
+                      Icons.refresh,
+                      color: Colors.black,
+                    ),
+                  ],
+                )
+              : const Text(
+                  "Get started",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
         )
       ],
     );
